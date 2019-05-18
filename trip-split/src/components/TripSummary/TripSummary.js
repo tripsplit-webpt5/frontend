606bc54021d52aea3b31cbf352a92d0bf7dc5415
Bharath <b.lavendra.95@gmail.com>
2019-05-18 17:06:09 -0500

    Trip Summary Page Designed

--- trip-split/src/components/TripSummary/TripSummary.js
+++ trip-split/src/components/TripSummary/TripSummary.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import TransactionDayList from '../TripSummary/TransactionDayList';
+import TripSummaryBar from './TripSummaryBar';
 
 class TripSummary extends Component{
     render() {
         return(
             <div>
                 <NavBar />
+                <TripSummaryBar />
+                <TransactionDayList />
             </div>
         )
     }
---
+++ trip-split/src/components/TripSummary/TripSummaryBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Close from '@material-ui/icons/Close';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function TripSummaryBar(props) {
+  const { classes } = props;
+  return (
+    <div>
+        $1,088 spent
+      <Button variant="contained" color="secondary" className={classes.button}>
+      <Close className={classes.rightIcon} />
+         End Trip
+      </Button>
+    </div>
+  );
+}
+
+TripSummaryBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TripSummaryBar);
