e16f1446ee4d3b7d5016a832e86da50230a2950f
Bharath <b.lavendra.95@gmail.com>
2019-05-19 20:17:13 -0500

    Trips array trial

--- trip-split/src/components/Trips/EditTrip.js
+++ trip-split/src/components/Trips/EditTrip.js
@@ -15,30 +15,42 @@ class EditTrip extends Component {
     constructor(props) {
         super (props);
         this.state = {
-        title: props.title,
+        title: '',
         user_id: localStorage.getItem("id"),
         trip_id: this.props.match.params.id,
-        number_travelers: props.number_travelers,
-        destination: props.destination,
-        dates: props.dates,
-        names: props.names
+        number_travelers: '',
+        destination: '',
+        dates: '',
+        names: '',
+        trip: ''
         }
     };
 
-    newTrip = event => {
+    componentDidMount () {
+      axios
+        .get(`https://trip-split-backend.herokuapp.com/user/trips/${this.state.trip_id}`, headers)
+        .then(response => {
+          this.setState({
+            trip: response.data
+          })
+        })
+        .catch(err => console.log(err))
+    }
+
+    editTrip = event => {
         event.preventDefault();
 
-        // axios
-        // .put(`https://trip-split-backend.herokuapp.com/user/trips/${props.id}`, this.state, headers)
-        // .then(response => {
-        //     console.log(response)
-        // },
-        // this.props.history.push('/trips'))
-        // .catch(err => console.log(err))
+        axios
+        .put(`https://trip-split-backend.herokuapp.com/user/trips/${this.state.trip_id}`, this.state, headers)
+        .then(response => {
+            console.log(response)
+        },
+        this.props.history.push('/trips'))
+        .catch(err => console.log(err))
     }
 
     handleInputChange = e => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({[this.trip.e.target.name]: this.trip.e.target.value});
     }
 
     render(){
@@ -47,7 +59,7 @@ class EditTrip extends Component {
       <Typography variant="h6" gutterBottom>
         Edit Trip
       </Typography>
-      <form >
+      <form onSubmit={this.editTrip}>
       <Grid container spacing={24}>
         <Grid item xs={12} sm={6}>
           <TextField
@@ -56,7 +68,7 @@ class EditTrip extends Component {
             name="title"
             label="Title"
             fullWidth
-            onChange={this.handleInputChange} value={this.state.title}
+            onChange={this.handleInputChange} value={this.state.trip.title}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -66,7 +78,7 @@ class EditTrip extends Component {
             name="destination"
             label="Destination"
             fullWidth
-            onChange={this.handleInputChange} value={this.state.destination}
+            onChange={this.handleInputChange} value={this.state.trip.destination}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -76,7 +88,7 @@ class EditTrip extends Component {
             name="dates"
             label="Dates"
             fullWidth
-            onChange={this.handleInputChange} value={this.state.dates}
+            onChange={this.handleInputChange} value={this.state.trip.dates}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -86,7 +98,7 @@ class EditTrip extends Component {
             name="number_travelers"
             label="Number of Travelers"
             fullWidth
-            onChange={this.handleInputChange} value={this.state.number_travelers}
+            onChange={this.handleInputChange} value={this.state.trip.number_travelers}
             />
         </Grid>
         <Grid item xs={12}>
@@ -96,7 +108,7 @@ class EditTrip extends Component {
             name="names"
             label="Names of Travelers"
             fullWidth
-            onChange={this.handleInputChange} value={this.state.names}
+            onChange={this.handleInputChange} value={this.state.trip.names}
           />
         </Grid>
         <Button
